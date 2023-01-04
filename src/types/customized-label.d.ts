156b8ee1d36237bb1c5e86d6109024461b434e5f
Arianna Johnson <ariannajohnson588@gmail.com>
2023-01-03 19:20:09 -0500

    add: customized label type

---
+++ src/types/customized-label.d.ts
@@ -0,0 +1,9 @@
+declare type CustomizedLabel = {
+    cx: number,
+    cy: number,
+    midAngle: number,
+    innerRadius: number,
+    outerRadius: number,
+    percent: number,
+    index: number,
+}
